1fbdeed6302a77be6132ad3ea67444d6e6d4f0eb
Gard Hillestad <gard.hillestad@gmail.com>
2016-12-21 22:34:19 +0100

    main function created

---
+++ bin/ino/main/main.ino
@@ -0,0 +1,41 @@
+
+#include <SPI.h>
+#include <MFRC522.h>
+
+
+
+#define RST_PIN         9          // Configurable, see typical pin layout above
+#define SS_PIN          10         // Configurable, see typical pin layout above
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);   // Initialize serial communications with the PC
+  while (!Serial);    // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
+  SPI.begin();      // Init SPI bus
+  mfrc522.PCD_Init();   // Init MFRC522
+  mfrc522.PCD_DumpVersionToSerial();  // Show details of PCD - MFRC522 Card Reader details
+  Serial.println(F("Test Running.... penis"));
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  // Look for new cards
+  // Look for new cards, and select one if present
+  if ( ! mfrc522.PICC_IsNewCardPresent() || ! mfrc522.PICC_ReadCardSerial() ) {
+    delay(50);
+    return;
+  }
+
+  Serial.println(F("Detected a card!"));
+
+  // Dump UID
+  Serial.print(F("Card UID:"));
+  for (byte i = 0; i < mfrc522.uid.size; i++) {
+    Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+    Serial.print(mfrc522.uid.uidByte[i], HEX);
+  } 
+  Serial.println();
+  
+}
