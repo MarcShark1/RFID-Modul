52243470b5c0b514d7a6c978c67d4c32319cc74b
Gard Hillestad <gard.hillestad@gmail.com>
2017-01-17 17:16:27 +0100

    fjernet rfid lib fra repo

--- bin/ino/main/main.ino
+++ bin/ino/main/main.ino
@@ -5,9 +5,14 @@
 
 
 #define RST_PIN         9          // Configurable, see typical pin layout above
-#define SS_PIN          53         // Configurable, see typical pin layout above
+#define SS_PIN          8         // Configurable, see typical pin layout above
 
 MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance
+
+// pin 12 is connected to the DataIn 
+// pin 11 is connected to the CLK 
+// pin 10 is connected to LOAD 
+// We have only a single MAX72XX.
 LedControl lc=LedControl(12,11,10,1);
 
 unsigned long delaytime=1000;
@@ -24,13 +29,12 @@ void setup() {
   lc.clearDisplay(0);
   // put your setup code here, to run once:
   Serial.begin(9600);   // Initialize serial communications with the PC
+  Serial1.begin(9600);
   while (!Serial);    // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
   SPI.begin();      // Init SPI bus
   mfrc522.PCD_Init();   // Init MFRC522
   mfrc522.PCD_DumpVersionToSerial();  // Show details of PCD - MFRC522 Card Reader details
   Serial.println(F("Test Running...."));
-  
-  Serial1.begin(9600)
 }
 
 void countDown(int negCount, int posCount){
@@ -39,22 +43,20 @@ void countDown(int negCount, int posCount){
      lc.setDigit(0,0,i,false);
      delay(1000);
   }
-  
   lc.setRow(0,2,0);
   for (int i=posCount; i+1 > 0; i--){
      lc.setDigit(0,0,i,false);
      delay(1000);
   }
-  
   lc.setRow(0,0,0);
   lc.setChar(0,2,'H',false);
   lc.setChar(0,1,'E',false);
   lc.setRow(0,0,6);
   delay(300);
-  lc.clearDisplay(0);
-  
+  lc.clearDisplay(0);  
 }
-void dumpUID{
+
+void dumpUID(){
   // Dump UID to Serial
   Serial.print(F("Card UID:"));
   for (byte i = 0; i < mfrc522.uid.size; i++) {
@@ -89,5 +91,4 @@ void loop() {
 
   delay(1000);
 
-  
 }
